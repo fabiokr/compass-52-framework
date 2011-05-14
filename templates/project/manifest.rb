ce62f8ae53db37d15dc40b5dd37c565622ec171b
Fabio Kreusch <fabiokr@gmail.com>
2011-05-14 13:33:03 -0700

    Added compass-52-framework Github link to manifest

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -9,10 +9,12 @@ help %Q{
 Please see the 52 Framework website for documentation:
 
     http://www.52framework.com/
+    https://github.com/fabiokr/compass-52-framework
 }
 
 welcome_message %Q{
 Please see the 52 Framework website for documentation:
 
     http://www.52framework.com/
+    https://github.com/fabiokr/compass-52-framework
 }
